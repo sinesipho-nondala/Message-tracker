52c3ea0a804b4c2b5e9e9f129ebc35ba0ae24836
sinesipho-nondala <ST10515267@rcconnect.edu.za>
2026-06-09 19:43:43 +0200

    Update MessageTracker.java

--- MessageTracker.java
+++ MessageTracker.java
@@ -7,4 +7,6 @@ private static List<String>sentMessage = new ArrayList<>();
 private static List<String>storeMessages = new ArrayList<>();
 private static List<String>disregardedMessages = new ArrayList<>();
 private static List<String>messageIDs = new ArrayList<>();
+private static List<String>messageHashes = new ArrayList<>();
+private static List<String>recipientList = new ArrayList<>();
   
