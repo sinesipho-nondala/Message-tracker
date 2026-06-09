8e9f5d5fa36e02d66e980e7cdef523ce511f250f
sinesipho-nondala <ST10515267@rcconnect.edu.za>
2026-06-09 19:35:00 +0200

    Add parallel arrays for message tracking

--- MessageTracker.java
+++ MessageTracker.java
@@ -1,2 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
+
+public class MessageTracker{
+  //Five parallel arrays
+private static List<String>sentMessage = new ArrayList<>();
+private static List<String>storeMessages = new ArrayList<>();
+private static List<String>disregardedMessages = new ArrayList<>();
+private static List<String>messageIDs = new ArrayList<>();
+private static List<String>messageHashes = new ArrayList<>();
+private static 
