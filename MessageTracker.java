0e3802013480255f76f1524bba1249ea8f8c9798
sinesipho-nondala <ST10515267@rcconnect.edu.za>
2026-06-09 19:41:59 +0200

    Add five parallel arrays for sent, stored, disregarded, hash, and ID tracking
    Removed unused lists for message hashes and recipient list.

--- MessageTracker.java
+++ MessageTracker.java
@@ -7,6 +7,4 @@ private static List<String>sentMessage = new ArrayList<>();
 private static List<String>storeMessages = new ArrayList<>();
 private static List<String>disregardedMessages = new ArrayList<>();
 private static List<String>messageIDs = new ArrayList<>();
-private static List<String>messageHashes = new ArrayList<>();
-private static List<String>recipientList = new ArrayList<>();
-}
+  
