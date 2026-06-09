71cb8c777df050cd922e88cc40c3afdeea7df7f5
sinesipho-nondala <ST10515267@rcconnect.edu.za>
2026-06-09 19:54:41 +0200

    Add menu option 4 with stored messages sub-menu

--- MessageTracker.java
+++ MessageTracker.java
@@ -9,5 +9,14 @@ private static List<String>disregardedMessages = new ArrayList<>();
 private static List<String>messageIDs = new ArrayList<>();
 private static List<String>messageHashes = new ArrayList<>();
 private static List<String>recipientList = new ArrayList<>();
+//New: Menu method
+public static void showMenu(){
+  System.out.println("\n===MENU===");
+  System.out.println("1)Send Messages);
+  System.out.println("2)Show recently sent messages");
+  System.out.println("3)Quit");
+  System.out.println("4)Stored Messages");
+}
+    
 }
   
