140adca81364ebc421eaba31f6791af2bb0dca52
sinesipho-nondala <ST10515267@rcconnect.edu.za>
2026-06-09 19:36:02 +0200

    Add recipientList to MessageTracker

--- MessageTracker.java
+++ MessageTracker.java
@@ -8,4 +8,5 @@ private static List<String>storeMessages = new ArrayList<>();
 private static List<String>disregardedMessages = new ArrayList<>();
 private static List<String>messageIDs = new ArrayList<>();
 private static List<String>messageHashes = new ArrayList<>();
-private static 
+private static List<String>recipientList = new ArrayList<>();
+}
